665e329c5de52403b516b11dc6f0f08b2b45a049
dw33xn <1067388454@qq.com>
2018-04-23 22:20:42 +0800

    对之前提交的更改

--- ServerMonitor/Controls/SMTPRequest.cs
+++ ServerMonitor/Controls/SMTPRequest.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace ServerMonitor.Controls
 {
@@ -35,6 +38,7 @@ namespace ServerMonitor.Controls
         /// <returns></returns>
         public override async Task<bool> MakeRequest()
         {
+            CreateTime = DateTime.Now;
             try
             {
                 Socket s = null;  //用他来建立连接，发送信息
@@ -51,37 +55,57 @@ namespace ServerMonitor.Controls
                     hostEndPoint = new IPEndPoint(hostAddress, 587);// get our end point
                     // prepare the socket
                     s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    s.ReceiveTimeout = OverTime;  //接收超时
-                    s.SendTimeout = OverTime;     //发送超时
+
+                    Stopwatch stopwatch = new Stopwatch(); // 记录请求耗时
+
+                    DispatcherTimer timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, OverTime) };
+                    timer.Tick += new EventHandler<object>((sender, e) =>
+                    {
+                        if (!s.Connected) // //Connection timed out...
+                        {
+                            Status = "1003";
+                            TimeCost = OverTime;
+                            ErrorException = new Exception("请求超时");
+                            s.Dispose();
+                            s = null;
+                        }
+                        timer.Stop();
+                    });
+                    
                     try
                     {
-                        s.Connect(hostEndPoint);   //建立连接
+                        timer.Start();
+                        await s.ConnectAsync(hostEndPoint);
                     }
                     catch (Exception e) //Connection timed out...
                     {
-                        Status = "1003"; // 收集捕获到的异常
-                        ErrorException = e; // 请求耗时设置为超时上限
-                        TimeCost = OverTime;
+                        if (s != null)
+                        {
+                            Status = "1003";
+                            ErrorException = e;
+                            TimeCost = OverTime;
+                        }
                         continue;
                     }
                     if (!s.Connected) // Connection failed, try next IPaddress. 当作超时处理
                     {
                         Status = "1003";
                         TimeCost = OverTime;
-                        s = null;
-                        continue;
+                        ErrorException = new Exception("未建立连接");
                     }
                     else
                     {
                         s.Receive(RecvFullMessage);//接受建立连接时的返回信息
                         //交代自己认证SMTP服务器的域名 然后发送 接收信息存在RecvFullMessage
-                        var dt = DateTime.Now;
+
+                        stopwatch.Start();
                         ByteCommand = ASCII.GetBytes("HELO " + DomainName + "\r\n");
                         s.Send(ByteCommand, ByteCommand.Length, 0);
                         s.Receive(RecvFullMessage);
+                        stopwatch.Stop();
 
                         Status = ASCII.GetString(RecvFullMessage).Substring(0, 3);
-                        TimeCost = (short)DateTime.Now.Subtract(dt).Milliseconds;
+                        TimeCost = (short)stopwatch.ElapsedMilliseconds;
                         ActualResult = ASCII.GetString(RecvFullMessage);
                         return true;
                     }
@@ -96,9 +120,7 @@ namespace ServerMonitor.Controls
                 ErrorException = e;
                 // 请求耗时设置为超时上限
                 TimeCost = OverTime;
-                return false;
             }
-
             return false;
         }
     }
