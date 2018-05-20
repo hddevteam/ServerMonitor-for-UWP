a3d91f9ad8324a4cce7b96af26127ee8245c538f
Bin <1510120026@vip.henu.edu.cn>
2018-05-20 09:30:24 +0800

    添加IAddServerUtil 以及AddServerUtilImpl 文件

---
+++ ServerMonitor/ViewModels/BLL/AddServerUtilImpl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerMonitor.ViewModels.BLL
+{
+    public class AddServerUtilImpl:IAddServerUtil
+    {
+    }
+}
