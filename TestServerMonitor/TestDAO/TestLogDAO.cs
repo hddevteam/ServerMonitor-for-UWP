0680e58dde84a3f72cd365b9f6c001e7bf1a2445
1158558425 <1610130044@vip.henu.edu.cn>
2018-05-29 20:29:14 +0800

    完善LogDAO测试，修改了TestLogDAO.cs文件

--- TestServerMonitor/TestDAO/TestLogDAO.cs
+++ TestServerMonitor/TestDAO/TestLogDAO.cs
@@ -90,6 +90,7 @@ namespace TestServerMonitor.TestDAO
         {
             Assert.AreEqual(1,logDAO.InsertOneLog(log));//插入log
             Assert.AreEqual(log.Id, logDAO.GetLogById(log.Id).Id);//log插入后获得id，然后看取到值的id，是否和log的id相等
+            logDAO.DeleteOneLog(log.Id);//清除插入数据
         }
         /// <summary>
         /// 测试插入一条空的log
@@ -112,7 +113,8 @@ namespace TestServerMonitor.TestDAO
         public void TestLogDAO_InsertListLog()
         {
             Assert.AreEqual(logModels.Count, logDAO.InsertListLog(logModels));
-            Assert.AreNotEqual(0, logDAO.GetLogsBySiteId(logModels[0].Site_id).Count);//按照插入的log的site_id进行查询看查到的是否为空的List<LogModel>
+            Assert.AreEqual(2, logDAO.GetLogsBySiteId(logModels[0].Site_id).Count);//按照插入的log的site_id进行查询看是否查到
+            logDAO.DeleteLogsBySite(88);//清除插入数据
 
         }
         /// <summary>
@@ -123,7 +125,10 @@ namespace TestServerMonitor.TestDAO
         public void TestLogDAO_GetAllLog()
         {
 
-            Assert.AreNotEqual(0, logDAO.GetAllLog().Count);//如果等于0，查出一个空的List<LogModel>
+            logDAO.InsertListLog(logModels);
+            Assert.AreEqual(2, logDAO.GetAllLog().Count);//如果等于0，查出一个空的List<LogModel>
+            Assert.AreEqual(logModels[0].Id, logDAO.GetAllLog()[0].Id);
+            logDAO.DeleteLogsBySite(88);//清除插入数据
         }
         /// <summary>
         /// 测试根据Id查询Log
@@ -133,8 +138,23 @@ namespace TestServerMonitor.TestDAO
         [Priority(2)]
         public void TestLogDAO_GetLogById()
         {
-            Assert.AreNotEqual(0, logDAO.GetLogById(logDAO.GetAllLog()[0].Id));
-            
+            logDAO.InsertListLog(logModels);
+            Assert.AreEqual(logModels[0].Id, logDAO.GetLogById(logModels[0].Id).Id);
+            logDAO.DeleteLogsBySite(88);//清除插入数据
+
+        }
+        /// <summary>
+        /// 测试根据不存在的Id查询Log
+        /// 说明：查询不存在id
+        /// </summary>
+        [TestMethod]
+        [Priority(2)]
+        public void TestLogDAO_GetLogById_Error_Id()
+        {
+            logDAO.InsertListLog(logModels);
+            Assert.AreEqual(0, logDAO.GetLogById(55).Id);//查找一个不存在的id
+            logDAO.DeleteLogsBySite(88);//清除插入数据
+
         }
         /// <summary>
         /// 用预先插入的Log的Site_id进行测试GetLogBySiteId
@@ -155,6 +175,17 @@ namespace TestServerMonitor.TestDAO
             };
             logDAO.InsertOneLog(testlog);
             Assert.AreEqual(testlog.Site_id, logDAO.GetLogsBySiteId(testlog.Site_id)[0].Site_id);
+            logDAO.DeleteOneLog(testlog.Id);//清除插入数据
+        }
+        /// <summary>
+        /// 测试查找一个不存在的site_id
+        /// </summary>
+        [TestMethod]
+        public void TestLogDAO_GetLogBySiteId_Error_site_id()
+        {
+            logDAO.InsertListLog(logModels);
+            Assert.AreEqual(0, logDAO.GetLogsBySiteId(55).Count);//查找Error_site_id
+            logDAO.DeleteLogsBySite(88);//清除插入数据
         }
         /// <summary>
         /// 测试删除一条Log
@@ -175,7 +206,16 @@ namespace TestServerMonitor.TestDAO
             };
             logDAO.InsertOneLog(testlog);
             Assert.AreEqual(1, logDAO.DeleteOneLog(testlog.Id));
-            Assert.AreEqual(0, logDAO.GetLogById(testlog.Id).Site_id);//取一下testlog，看是否取到空log
+            Assert.AreEqual(0, logDAO.GetLogById(testlog.Id).Id);//取一下testlog，看是否取到空log   
+        }
+        /// <summary>
+        /// 测试删除一条不存在的id
+        /// </summary>
+        [TestMethod]
+        public void TestLogDAO_DeleteOneLog_NotExist_Id()
+        {
+            Assert.AreEqual(0, logDAO.DeleteOneLog(55));
+
         }
         /// <summary>
         /// 用预先插入的Log的Site_id进行测试根据site_id进行删除
@@ -196,8 +236,14 @@ namespace TestServerMonitor.TestDAO
             logDAO.InsertOneLog(testlog);
             Assert.AreEqual(1, logDAO.DeleteLogsBySite(testlog.Site_id));
             Assert.AreEqual(0,logDAO.GetLogById(testlog.Id).Id);//取一下testlog，看取得的是否为空log
-
-            
+        }
+        /// <summary>
+        /// 查询一条不存在的site_id
+        /// </summary>
+        [TestMethod]
+        public void TestLogDAO_DeleteLogsBySite_Error_site_id()
+        {
+            Assert.AreEqual(0, logDAO.DeleteLogsBySite(55));
         }
         /// <summary>
         /// 测试更新一条Log
@@ -218,8 +264,11 @@ namespace TestServerMonitor.TestDAO
             };
             logDAO.InsertOneLog(testlog);//将testlog插入
             testlog.Is_error = true;//改变testlog的Is_error为true
+            testlog.Log_Record = "bug";//改变testlog的Log_Record为“bug”
             logDAO.UpdateLog(testlog);
             Assert.AreEqual(true, logDAO.GetLogById(testlog.Id).Is_error);//对比updatelog后testlog的Is_error是否改变
+            Assert.AreEqual("bug", logDAO.GetLogById(testlog.Id).Log_Record);//对比updatelog后的log_record是否改变
+            logDAO.DeleteOneLog(testlog.Id);//清除插入数据
         }
         /// <summary>
         /// 测试更新一个Log列表
@@ -230,8 +279,11 @@ namespace TestServerMonitor.TestDAO
         {
             logDAO.InsertListLog(logModels);
             logModels[0].Log_Record = "ok";//将插入的logmodel[0]的Log_Record字段改为ok
+            logModels[1].Is_error = false;//将logmodel[1]的is_error字段改为false
             logDAO.UpdateListLog(logModels);
             Assert.AreEqual("ok", logDAO.GetLogById(logModels[0].Id).Log_Record);//对比Log_Record字段是否发生改变
+            Assert.AreEqual(false, logDAO.GetLogById(logModels[1].Id).Is_error);
+            logDAO.DeleteLogsBySite(88);//清除插入数据
 
         }
         /// <summary>
@@ -260,8 +312,25 @@ namespace TestServerMonitor.TestDAO
         public void TestLogDAO_DBExcuteLogCommand()
         {
             object[] o = new object[] { };
-            Assert.AreNotEqual(null, logDAO.DBExcuteLogCommand("select *from Log", o));
+            Assert.AreNotEqual(null, logDAO.DBExcuteLogCommand("select *  from Log", o));
         }
+        /// <summary>
+        /// 测试执行一条错误的语句，测试DBExcuteLogCommand是否异常
+        /// </summary>
+        [TestMethod]
+        public void TestLogDAO_DBExcuteLogCommand_Command_error()
+        {
+            object[] o = new object[] { };
+            try
+            {
+                logDAO.DBExcuteLogCommand("select Sitee from Log", o);//执行一条错误语句
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("SQLite.Net.SQLiteException", ex.GetType().ToString());//看异常是否为SQLiteException
 
+            }
+            
+        }
     }
 }
