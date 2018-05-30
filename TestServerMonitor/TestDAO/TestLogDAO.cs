fb5df511330a07f3ad2d18ee2ac4c5e77b9e6737
liuyang <1324457514@qq.com>
2018-05-30 19:55:12 +0800

    Update TestLogDAO.cs

--- TestServerMonitor/TestDAO/TestLogDAO.cs
+++ TestServerMonitor/TestDAO/TestLogDAO.cs
@@ -30,11 +30,7 @@ namespace TestServerMonitor.TestDAO
         LogModel log;
 
         [TestMethod]
-<<<<<<< Updated upstream
         [ClassInitialize]
-=======
-        [ClassInitialize()]
->>>>>>> Stashed changes
         public static void InitDatabase(TestContext testContext)
         {
             DBInit db = DataBaseControlImpl.Instance;
